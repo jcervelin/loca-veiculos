b61bfa80cd1a5ef531a086a80f410a79bb855269
F0435936 Alexandre Henrique Magalhaes Ferreira <alexandre.ferreira@bb.com.br>
2022-11-16 15:31:19 -0300

    calculos de salario

--- src/main/java/solid/Funcionario.java
+++ src/main/java/solid/Funcionario.java
@@ -1,55 +1,52 @@
 package solid;
-
 import lombok.Data;
 import solid.entity.FuncionarioEntity;
-
 // Definicao de horas trabalhadas = horas diarias (max 8 horas por dia) + horas extras
-
 @Data
 public class Funcionario {
-
     private static final int MAX_HORAS_DIARIAS = 8;
-
     private static FuncionaroRepository repo;
-
     private String id;
     private String nome;
     private int horasDiarias;
     private int horasExtras;
     private double salarioHora;
-
-    public int totalHorasTrabalhadas () {
+    public int totalHorasTrabalhadas() {
         return horasDiarias + horasExtras;
     }
-
     // valor incluido superior a 8 é calculado como hora extra
     // valor incluido inferior a 8, a diferenca deverá ser descontada
-    // é chamado uma vez ao dia
-    public void incluirHorasDiarias(int horas) {
-
+    public void incluirHorasDiarias(int horasDiarias) {
+        this.horasDiarias += horasDiarias;
+        if (horasDiarias > MAX_HORAS_DIARIAS) {
+            this.horasExtras += horasDiarias - MAX_HORAS_DIARIAS;
+        } else if (horasDiarias < MAX_HORAS_DIARIAS) {
+            this.horasDiarias -= ((MAX_HORAS_DIARIAS - horasDiarias));
+        }
     }
-
     // Implementar logica para salario:
     // totalHorasTrabalhadas() * salario por hora
     public double calcularSalario() {
-        return -1;
+        return (totalHorasTrabalhadas() * salarioHora);
     }
-
     // Implementar logica para salario:
     // (horasExtras * (salarioHora * 0.5))
     public double calcularHoraExtra() {
-        return -1;
+        return (horasExtras * (salarioHora * 0.5));
     }
-
     // as horas reportadas sao horasDiarias + horasExtras
     public int reportarHorasTrabalhadas() {
-        return -1;
+        return horasDiarias + horasExtras;
     }
-
     public void save(Funcionario funcionario) {
         // converte funcionario para funcionarioEntity
         FuncionarioEntity entity = new FuncionarioEntity();
         entity.setId(funcionario.getId());
         repo.save(entity);
+    }
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario();
+        f.incluirHorasDiarias(9);
+        System.out.println(f.horasDiarias);
     }
 }
